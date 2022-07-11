4f0d9b70397230fc484c3055c07776e362bd9a4c
namvandy <woojinsong@Woojinui-MacBookAir.local>
2022-07-11 16:07:27 +0900

    secretkey update

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,8 @@ environ.Env.read_env(env_file = os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ggc*zq=u+m0p1sobctp!(a^wti)soq4241=0jb1@(3o9n!&df'
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
